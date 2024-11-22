654bf4473163761594c4ab703679ab13cb6b44a2
Saeed Mansourian <saeedcmk@yahoo.com>
2024-11-22 21:00:57 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="flex min-h-screen w-full flex-col items-center justify-center gap-12">
+			<h1 className="text-6xl">Kimia Derazgisoo</h1>
+			<h2 className="text-4xl text-blue-600">DATA ANALYST</h2>
+		</div>
+	);
+}
+
+export default App;
